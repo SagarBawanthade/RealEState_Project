edd70798f6c7f9192cd376ad4ca940a70c22094a
SagarBawanthade <sagar.bawanthade2004@gmail.com>
2024-01-05 11:48:15 +0530

    created signup api

--- api/index.js
+++ api/index.js
@@ -2,9 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 
 
 //mongoDB connection
@@ -15,8 +17,9 @@ mongoose.connect(process.env.MONGO_URI)
         console.log(error)
     })
 
-
+//routes
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => {
     console.log("Server is running on port no. 3000");
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,5 +19,5 @@ const userSchema = new mongoose.Schema({
     }
 }, { timestamps: true });
 
-const user = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 export default User;
