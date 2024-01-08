0aa84f192350d3485941baa97d8543d673bd8fbe
SagarBawanthade <sagar.bawanthade2004@gmail.com>
2024-01-08 13:17:17 +0530

    added prefix installation

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
-
+import path from "path";
 import cookieParser from "cookie-parser";
 import listingRouter from './routes/listing.route.js';
 
@@ -18,7 +18,9 @@ mongoose.connect(process.env.MONGO_URI)
         console.log("MongoDB Connected Successfully")
     }).catch((error) => {
         console.log(error)
-    })
+    });
+
+const __dirname = path.resolve();
 
 app.listen(3000, () => {
     console.log("Server is running on port no. 3000");
@@ -30,6 +32,12 @@ app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 
 //middlewares
